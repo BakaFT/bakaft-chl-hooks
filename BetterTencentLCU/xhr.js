ee5e3fabc1d1ec59df9075ed9280b61c0a61ef29
BakaFT <bakaft@outlook.com>
2023-08-25 11:31:15 +0800

    Init

---
+++ BetterTencentLCU/xhr.js
@@ -0,0 +1,42 @@
+export default[
+    {
+        // 开启聊天小窗 打开个人状态
+        matcher: "/lol-platform-config/v1/namespaces/LcuSocial",
+        preSend: (XhrRequestConfig) => {},
+        postSend: (response) => {
+            const respJson = JSON.parse(response.response)
+            respJson["ChatWindowPopoutEnabled"] = true
+            respJson["StatusesDisabled"] = false
+            response.response = JSON.stringify(respJson)
+            
+        }
+    },
+    {
+        // 开启炫彩页面
+        matcher: "/lol-platform-config/v1/namespaces/LCUCollections/LCUCollectiblesChromasEnabled",
+        preSend: (XhrRequestConfig) => {},
+        postSend: (response) => {
+            response.response = true
+        }
+    },
+    {
+        // 无视生涯隐私
+        matcher:"/lol-summoner/v1/profile-privacy-enabled",
+        preSend: (XhrRequestConfig) => {},
+        postSend: (response) => {
+            response.response = "FUCK U TENCENT"
+        }
+    },
+    {
+        // 批量分解开关
+        matcher: "/lol-loot/v1/mass-disenchant/configuration",
+        preSend: (XhrRequestConfig) => {},
+        postSend: (response) => {
+            const respJson = JSON.parse(response.response)
+            respJson["enabled"] = true
+            respJson["maxLootItemsSizeMassCraft"] = "99999"
+            response.response = JSON.stringify(respJson)
+            
+        }
+    }
+]
---
+++ README.md
@@ -0,0 +1,3 @@
+# BetterTencentLCU
+
+It's Tencent Server only so ignore it if you are playing on global server:)
