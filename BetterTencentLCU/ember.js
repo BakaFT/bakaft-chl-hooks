9db598914662f2ee2cc00125b8ebefc65ee37811
BakaFT <bakaft@outlook.com>
2023-09-01 09:30:22 +0800

    108 Add navbarDisplayMode to ember

--- BetterTencentLCU/ember.js
+++ BetterTencentLCU/ember.js
@@ -1,1 +1,13 @@
-export default []
+const NAVBAR_TEXTONLY = [
+    {
+        // 关闭导航栏文字
+        matcher: 'main-navigation-menu-item',
+        mixin: (Ember, args) => ({
+            shouldDisplayTextBelowIcons: false,
+        })
+    }
+]
+
+export default [
+    ...NAVBAR_TEXTONLY
+]
