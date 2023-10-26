49408b1b68c5dce3740c56f0282c90840943da0b
BakaFT <bakaft@outlook.com>
2023-10-26 17:09:40 +0800

    Remove topup of currency

--- BetterTencentLCU/ember.js
+++ BetterTencentLCU/ember.js
@@ -1,13 +1,23 @@
-const NAVBAR_TEXTONLY = [
+const NAVBAR = [
     {
         // 关闭导航栏文字
         matcher: 'main-navigation-menu-item',
         mixin: (Ember, args) => ({
             shouldDisplayTextBelowIcons: false,
         })
+    },
+    {
+        // 关闭点券充值按钮
+        matcher: "currency-container-stacked",
+        mixin: () => ({
+            isRPTopUpEnabled: false,
+            didRender() {
+                this.set("isRPTopUpEnabled", false);
+            }
+        })
     }
 ]
 
 export default [
-    ...NAVBAR_TEXTONLY
+    ...NAVBAR
 ]
