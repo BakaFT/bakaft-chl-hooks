d9dc08fe9fd55de6eea330c3ab4a91dcc8e8bd68
BakaFT <bakaft@outlook.com>
2023-10-27 13:12:28 +0800

    Revert "Remove topup of currency"
    This reverts commit 49408b1b68c5dce3740c56f0282c90840943da0b.

--- BetterTencentLCU/ember.js
+++ BetterTencentLCU/ember.js
@@ -1,23 +1,13 @@
-const NAVBAR = [
+const NAVBAR_TEXTONLY = [
     {
         // 关闭导航栏文字
         matcher: 'main-navigation-menu-item',
         mixin: (Ember, args) => ({
             shouldDisplayTextBelowIcons: false,
         })
-    },
-    {
-        // 关闭点券充值按钮
-        matcher: "currency-container-stacked",
-        mixin: () => ({
-            isRPTopUpEnabled: false,
-            didRender() {
-                this.set("isRPTopUpEnabled", false);
-            }
-        })
     }
 ]
 
 export default [
-    ...NAVBAR
+    ...NAVBAR_TEXTONLY
 ]
